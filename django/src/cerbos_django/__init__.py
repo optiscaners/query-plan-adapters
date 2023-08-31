a820163e9a5c436c7998834352d9ab0373752d3a
Jakob Riebe <jr@estino.de>
2023-08-31 18:33:29 +0200

    feature: export types used for `get_queryset`

--- django/src/cerbos_django/__init__.py
+++ django/src/cerbos_django/__init__.py
@@ -1,7 +1,7 @@
 import importlib.metadata
 
-from cerbos_django.query import get_queryset
+from cerbos_django.query import get_queryset, GenericAttribute, OperatorFnMap
 
 __version__ = importlib.metadata.version(__package__ or __name__)
 
-__all__ = ["get_queryset"]
+__all__ = ["get_queryset", "GenericAttribute", "OperatorFnMap"]
